648324c16198ad1746dfad387caf67ea23905558
Cristóbal Peñaloza <cristobal.tolin@gmail.com>
2026-02-04 00:33:28 -0300

    Actualización a ultima versión de flutter

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\crist\AndroidStudioProjects\charts\charts_flutter\example"
+export "FLUTTER_ROOT=C:\Users\Cristobal\develop\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Cristobal\Repos\AndroidStudioProjects\charts\charts_flutter\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
